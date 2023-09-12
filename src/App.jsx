71b324be40e08d694e97bcc0daba410e45d7a784
PaolaaR <p.riveranovoa@gmail.com>
2023-09-11 22:59:57 -0300

    First commit with Header and Footer

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header"
+import { Footer } from "./components/Footer.jsx"
+
+
+export const App = () => {
+  return (
+    <>
+     <Header/>
+      <Footer/>
+    </>
+   
+
+  )
+}
+export default App
